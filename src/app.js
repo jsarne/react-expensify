a4717d67e3205dbcd2d6c32e39545fc6c51f286a
James Sarne <jim.sarne@gmail.com>
2020-01-28 10:19:23 -0800

    remove test data from app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 
 // normalize display across browsers - i.e. CSS reset
 import 'normalize.css/normalize.css';
@@ -11,11 +10,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const myStore = configureStore();
-
-// some testing - default sort order is date most recent at top
-myStore.dispatch(addExpense({description: 'water bill', amount: 4500, createdAt: 12345}));
-myStore.dispatch(addExpense({description: 'gas bill', amount: 1000, createdAt:   23456}));
-myStore.dispatch(addExpense({description: 'rent', amount: 109500, createdAt:     10000}));
 
 const jsx = (
   <Provider store={myStore}>
