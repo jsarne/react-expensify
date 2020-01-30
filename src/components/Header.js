f2ca335496025c16f91a931c9c23c58fe3493168
James Sarne <jim.sarne@gmail.com>
2020-01-30 12:08:04 -0800

    fixed dashboard w/public route, removed help page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import {startLogout} from '../actions/auth';
 export const Header = ({logout}) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active">Home</NavLink>
     <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
     <button onClick={logout}>Logout</button>
   </header>
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Route, Redirect} from 'react-router-dom';
-import Header from '../components/Header';
 
-export const PrivateRoute = ({isAuthenticated, component: Component, ...otherProps}) => (
+export const PublicRoute = ({isAuthenticated, component: Component, ...otherProps}) => (
   <Route {...otherProps} component = {(props) => (
     isAuthenticated ? (
-      <div>
-        <Header />
-        <Component {...props} />
-      </div>
+      <Redirect to="/dashboard" />
     ) : (
-      <Redirect to="/" />
+      <Component {...props} />
     )
   )}/>
 );
@@ -20,4 +16,4 @@ const mapStateToProps = (state) => ({
   isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PublicRoute);
