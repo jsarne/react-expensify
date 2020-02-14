be789dc036d0e4eb9afbb1a130db7f4b9b7b7c25
James Sarne <jim.sarne@gmail.com>
2020-02-13 22:54:28 -0800

    add confirm modal for expense deletion

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Modal from 'react-modal';
 import ExpenseForm from './ExpenseForm';
 import {startEditExpense, startRemoveExpense} from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
+  state = {
+    modalIsOpen: false
+  }
+
   onSubmit = (expense) => {
     this.props.editExpense(this.props.match.params.id, expense);
     this.props.history.push('/');
   }
 
   onRemoveClick = () => {
+    this.setState(() => ({modalIsOpen: true}));
+  }
+
+  onModalNo = () => {
+    this.setState(() => ({modalIsOpen: false}));
+  }
+
+  onModalYes = () => {
+    this.setState(() => ({modalIsOpen: false}));
     this.props.removeExpense(this.props.match.params.id);
     this.props.history.push('/');
   }
 
   render() {
+    Modal.setAppElement(document.getElementById('app'));
     return (
       <div>
         <div className="page-header">
@@ -24,10 +39,20 @@ export class EditExpensePage extends React.Component {
         </div>
         <div className="content-container">
           <ExpenseForm source="edit" expense={this.props.expense} onSubmit={this.onSubmit} />
-          <button className="button button--secondary" onClick={this.onRemoveClick}>Remove Expense</button>
+          <button name="removeButton" className="button button--secondary" onClick={this.onRemoveClick}>Remove Expense</button>
         </div>
-        <div>
-        </div>
+        <Modal className="modal"
+               isOpen={this.state.modalIsOpen}
+               shouldCloseOnEsc={true}
+               shouldCloseOnOverlayClick={true}
+               onRequestClose={this.onModalNo}>
+          <h2 className="modal__title">Delete Expense</h2>
+          <p>Are you sure?</p>
+          <div className="modal__buttons">
+            <button className="button" name="removeYes" onClick={this.onModalYes}>Yes</button>
+            <button className="button button--secondary" name="removeNo" onClick={this.onModalNo}>No</button>
+          </div>
+        </Modal>
       </div>
     );  
   }
